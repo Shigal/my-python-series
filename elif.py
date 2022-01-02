86a633d6f4724560468b9fd5a36147d2eb101239
ShigLaws <shiglaws@gmail.com>
2022-01-02 18:49:16 +0530

    if and else if statements example

---
+++ elif.py
@@ -0,0 +1,14 @@
+# elseif is given as elif
+
+player = "Dhoni"
+
+if player == "Kholi":
+    print("It's Kholi")
+elif player == "ABD":
+    print("It's ABD")
+elif player == "Rohit":
+    print("It's Rohit")
+elif player == "Dhoni":
+    print("It's Dhoni")
+else:
+    print("Hello!")
