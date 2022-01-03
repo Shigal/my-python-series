6e5af34b8b649ef229be833245593b78a7b4bc11
ShigLaws <shiglaws@gmail.com>
2022-01-03 20:11:38 +0530

    example for input function

---
+++ input_1.py
@@ -0,0 +1,8 @@
+# input - a built-in function to get data from the user
+
+age = input("How old are you?")
+age = int(age)
+if age < 25:
+    print("Hey, you are young")
+else:
+    print("Hey, you are not so young")
