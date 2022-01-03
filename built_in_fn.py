3f211a66ad286d1825e47cf431c875d4917cca78
ShigLaws <shiglaws@gmail.com>
2022-01-03 20:11:07 +0530

    examples for built-in functions:len, type, str, int, float

---
+++ built_in_fn.py
@@ -0,0 +1,27 @@
+# built-in functions: len(), type(), str(), int(), float()
+
+l = len("Programming is fun")
+print(l)
+
+t1 = type(23)
+print(t1)
+
+t2 = type('3')
+print(t2)
+
+t3 = type("hello")
+print(t3)
+
+t4 = type(3.21)
+print(t4)
+
+s = str(1000)
+print(type(s))
+
+i = int("200")
+print(type(i))
+
+f = float("300")
+print(f)
+
+
