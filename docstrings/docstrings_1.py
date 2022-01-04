d8ae75c4860634ac3a1b224426125a5261adb968
ShigLaws <shiglaws@gmail.com>
2022-01-04 19:04:36 +0530

    use Docstrings in the program

---
+++ docstrings/docstrings_1.py
@@ -0,0 +1,14 @@
+# Docstrings
+
+def multipy(a,b):
+    """
+    Returns a*b.
+    :param a: int first integer to be multiplied.
+    :param b: int second integer to be multiplied.
+    :return: int multiplication of a & b.
+    """
+    
+    return a*b
+
+# Here, docstrings lists all parameters of the function, its return value, and
+# the datatype of all parameter and return value
