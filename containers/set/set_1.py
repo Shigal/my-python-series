f299ecdb5dee5243df493236c795a7fa6cab16a2
ShigLaws <shiglaws@gmail.com>
2022-01-06 21:03:33 +0530

    example of set-container

---
+++ containers/set/set_1.py
@@ -0,0 +1,8 @@
+# creating a set
+my_set_1 = set(['Java', 'Python', 'Java', 'Python', 'JavaScript'])
+print (my_set_1) # {'JavaScript', 'Java', 'Python'}
+
+my_set_2 = set((3, 4, 5, 2, 5, 3, 4, 5))
+print(my_set_2) # {2, 3, 4, 5}
+
+
