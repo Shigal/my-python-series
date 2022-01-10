843371c00aaae86f7db1b4cf253a0a063e3cecd3
ShigLaws <shiglaws@gmail.com>
2022-01-10 20:40:19 +0530

    upper, lower, capitalize methods in string

---
+++ strings/string_1.py
@@ -0,0 +1,21 @@
+"""
+To write
+in more than
+one line
+"""
+
+my_string = "HELLO"
+print(my_string[0])
+
+# using upper method
+print("""The best way to study coding is by actually practice coding everyday.
+Never stop learning""" .upper())
+
+# using lower method
+print("""The best way to study coding is by actually practice coding everyday.
+Never stop learning""" .lower())
+
+# using captitalize method - only first letter capitalized
+print("""The best way to study coding is by actually practice coding everyday.
+Never stop learning""" .capitalize())
+
