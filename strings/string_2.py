d2bc81795c25c491fd9e5600e082c8d7318371cb
ShigLaws <shiglaws@gmail.com>
2022-01-10 20:40:50 +0530

    format function in string

---
+++ strings/string_2.py
@@ -0,0 +1,8 @@
+# format function
+
+year_created = "1989"
+print("Python was created in {}.".format(year_created)) # Python was created in 1989.
+
+creator = "Guido van Rossum"
+country = "Netherlands"
+print("Python was created in {} by {} in the {}.".format(year_created, creator, country))
