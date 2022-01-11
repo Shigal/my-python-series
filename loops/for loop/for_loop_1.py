8501835889b8d18dd60cc68f4907f381bbc28f29
ShigLaws <shiglaws@gmail.com>
2022-01-11 18:57:19 +0530

    examples for for-loops

---
+++ loops/for loop/for_loop_1.py
@@ -0,0 +1,9 @@
+# for loop
+
+for i in range(0, 10):
+    print("Python - {}".format(i))
+
+# when i=10, nothing will print, because i=number to stop at
+# Python moves to the code after the loop
+
+
---
+++ loops/for loop/for_loop_2.py
@@ -0,0 +1,24 @@
+# Iterable - String
+my_string = "python"
+for character in my_string:
+    print(character)
+
+
+# Iterable - list
+my_list = ["Java", "Python", "JavaScript"]
+for item in my_list:
+    print(item)
+
+
+# Iterable - tuple
+my_tuple = ('AI', 'ML', 'DS')
+for item in my_tuple:
+    print(item)
+
+
+
+# Iterable - dictionary
+my_dict = {'Datastructure & Algorithms':'D&A', 'Computer Networking': 'CN', 'Data Security': 'DS'}
+for key in my_dict:
+    print(key)
+# when we iterate through a dictionary, we can only access each key in it, not values
