122766b4be44f3d27bc6628e62062de397b54529
ShigLaws <shiglaws@gmail.com>
2022-01-13 19:22:19 +0530

    renamed folders

---
+++ [010]modules/module_1.py
@@ -0,0 +1,10 @@
+# math module comes with Python when we install it
+# a regular python file comes with a number of math related functionality
+# once imported can use any of the code from it
+# fabs function returns the absolute value of the parameter we pass in
+
+import math
+
+val = math.fabs(-3)
+print(val) # 3.0
+
