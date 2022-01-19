40ad64b123b9def3169356a21437cad94001b11a
ShigLaws <shiglaws@gmail.com>
2022-01-19 18:44:35 +0530

    classes are objects in Python

---
+++ [013]OOP/class-r-object/object_1.py
@@ -0,0 +1,13 @@
+# when we run a program in which we define a class - Python turns it into an object
+# Python turns our class definition into an object which can then be used in our program
+
+
+class Scavenger:
+    pass
+
+print(Scavenger)
+
+
+# Output:
+# <class '__main__.Scavenger'>
+
